9bbab080be73920ec6cb5681ea4cbb42f73f2d9b
Luigi Poole <luigipoole@outlook.com>
2015-12-16 19:19:53 -0800

    fixes

--- tests/enable.js
+++ tests/enable.js
@@ -4,8 +4,7 @@ var nodeHotspot = require('../index');
 nodeHotspot.enable({
     ssid: 'node-hotspot',
     password: 'default2710',
-    force: true,
-    adaptor: 'Ethernet'
+    force: true
 })
     .then(console.log.bind(this, 'Test Hotspot enabled!'))
     .catch(console.error)
