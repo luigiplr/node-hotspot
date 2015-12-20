8d1124945d23de27ad856bbac131ebad8c673e2f
Luigi Poole <luigipoole@outlook.com>
2015-12-19 16:11:23 -0800

    bump

--- build/utils/windows.js
+++ build/utils/windows.js
@@ -22,48 +22,59 @@ var _child_process2 = _interopRequireDefault(_child_process);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var getPowershell = function getPowershell() {
-    return _path2.default.join(process.env.windir, process.env.PROCESSOR_ARCHITECTURE === 'AMD64' ? 'System32/WindowsPowerShell/v1.0/powershell.exe' : 'SysWOW64/WindowsPowerShell/v1.0/powershell.exe');
-};
+var arch = false;
 
 module.exports = {
-    enable: function enable(opts) {
+    getPowershell: function getPowershell() {
+        return _path2.default.join(process.env.windir, arch === 'x64' ? 'SysWOW64/WindowsPowerShell/v1.0/powershell.exe' : 'System32/WindowsPowerShell/v1.0/powershell.exe');
+    },
+    getArch: function getArch() {
         var _this = this;
+
+        return new _bluebird2.default(function (resolve, reject) {
+            _this.exec('wmic os get osarchitecture').then(function (osArch) {
+                if (osArch.split('\n')[1].match(/64/)) arch = 'x64';else arch = 'ia32';
+
+                resolve();
+            }).catch(reject);
+        });
+    },
+    enable: function enable(opts) {
+        var _this2 = this;
 
         return new _bluebird2.default(function (resolve, reject) {
 
             if (opts.force) {
                 opts.force = false;
-                return _this.disable().then(_this.enable.bind(_this, opts)).then(resolve).catch(reject);
+                return _this2.disable().then(_this2.enable.bind(_this2, opts)).then(resolve).catch(reject);
             }
-
-            _this.create(opts.ssid, opts.password).then(function () {
+            _this2.getArch().then(_this2.create.bind(_this2, opts.ssid, opts.password)).then(function () {
                 return new _bluebird2.default(function (resolve, reject) {
                     if (opts.adaptor) {
-                        _this.ConnectedAdaptor = opts.adaptor;
+                        _this2.ConnectedAdaptor = opts.adaptor;
                         return resolve();
                     }
                     if (opts.adaptor === false) {
-                        _this.ConnectedAdaptor = false;
+                        _this2.ConnectedAdaptor = false;
                         return resolve();
                     }
-                    _this.getInternetConnectedAdaptor().then(function (ConnectedAdaptor) {
-                        _this.ConnectedAdaptor = ConnectedAdaptor.InterfaceAlias;
+                    _this2.getInternetConnectedAdaptor().then(function (ConnectedAdaptor) {
+                        _this2.ConnectedAdaptor = ConnectedAdaptor.InterfaceAlias;
                         resolve();
                     }).catch(reject);
                 });
             }).then(function () {
                 console.log('Starting hotspot');
-                return _this.exec('netsh wlan start hostednetwork');
-            }).then(_this.getNetworkAdaptors.bind(_this)).then(function (NetworkAdaptors) {
+                return _this2.exec('netsh wlan start hostednetwork');
+            }).then(_this2.getNetworkAdaptors.bind(_this2)).then(function (NetworkAdaptors) {
                 console.log('Hotspot started!');
                 return _lodash2.default.result(_lodash2.default.find(NetworkAdaptors, function (adaptor) {
                     return adaptor.interface === 'Microsoft Hosted Network Virtual Adapter';
                 }), 'name');
             }).then(function (hostedNetwork) {
                 return new _bluebird2.default(function (resolve, reject) {
-                    if (_this.ConnectedAdaptor) _lodash2.default.delay(function () {
-                        _this.exec(_util2.default.format('"%s" enable "%s" "%s" true', _path2.default.join(__dirname, '../../', 'bin/win32/IcsManager.exe'), _this.ConnectedAdaptor, hostedNetwork)).then(resolve).catch(reject);
+                    if (_this2.ConnectedAdaptor) _lodash2.default.delay(function () {
+                        _this2.exec(_util2.default.format('"%s" enable "%s" "%s" true', _path2.default.join(__dirname, '../../', 'bin/win32/IcsManager.exe'), _this2.ConnectedAdaptor, hostedNetwork)).then(resolve).catch(reject);
                     }, 500);else resolve();
                 });
             }).then(function () {
@@ -72,19 +83,19 @@ module.exports = {
         });
     },
     disable: function disable(opts) {
-        var _this2 = this;
+        var _this3 = this;
 
         return new _bluebird2.default(function (resolve, reject) {
-            _this2.exec('netsh wlan stop hostednetwork').then(function () {
-                return _this2.exec('netsh wlan set hostednetwork mode=disallow');
+            _this3.exec('netsh wlan stop hostednetwork').then(function () {
+                return _this3.exec('netsh wlan set hostednetwork mode=disallow');
             }).then(resolve).catch(reject);
         });
     },
     getStatus: function getStatus() {
-        var _this3 = this;
+        var _this4 = this;
 
         return new _bluebird2.default(function (resolve, reject) {
-            _bluebird2.default.all([_this3.exec('netsh wlan show hostednetwork'), _this3.getNetworkAdaptors(), _this3.getInternetConnectedAdaptor(), _this3.getCompatible()]).spread(function (status, NetworkAdaptors, ConnectedAdaptor, Compatible) {
+            _this4.getArch().then(_bluebird2.default.all.bind(_this4, [_this4.exec('netsh wlan show hostednetwork'), _this4.getNetworkAdaptors(), _this4.getInternetConnectedAdaptor(), _this4.getCompatible()])).spread(function (status, NetworkAdaptors, ConnectedAdaptor, Compatible) {
                 var output = status.split('Hosted network settings')[1].replace('-----------------------', '').split('Hosted network status')[0].split('\n').map(Function.prototype.call, String.prototype.trim).filter(Boolean).concat(status.split('Hosted network status')[1].replace('---------------------', '').split('\n').map(Function.prototype.call, String.prototype.trim).filter(Boolean));
 
                 var statusObject = {};
@@ -100,10 +111,10 @@ module.exports = {
         });
     },
     getCompatible: function getCompatible() {
-        var _this4 = this;
+        var _this5 = this;
 
         return new _bluebird2.default(function (resolve, reject) {
-            _this4.exec('netsh wlan show drivers').then(function (output) {
+            _this5.exec('netsh wlan show drivers').then(function (output) {
                 var networkData = output.split('\n').map(Function.prototype.call, String.prototype.trim).filter(Boolean);
 
                 var matches = _lodash2.default.filter(networkData, function (line) {
@@ -114,10 +125,10 @@ module.exports = {
         });
     },
     getNetworkAdaptors: function getNetworkAdaptors() {
-        var _this5 = this;
+        var _this6 = this;
 
         return new _bluebird2.default(function (resolve, reject) {
-            _this5.exec(_util2.default.format('"%s" "Get-NetAdapter | ft Name, Status, ifIndex, MacAddress, InterfaceDescription"', getPowershell())).then(function (output) {
+            _this6.exec(_util2.default.format('"%s" "Get-NetAdapter | ft Name, Status, ifIndex, MacAddress, InterfaceDescription"', _this6.getPowershell())).then(function (output) {
                 var networkData = output.split('--------------------')[1].split('\n').map(Function.prototype.call, String.prototype.trim).filter(Boolean);
 
                 var networkAdaptors = [];
@@ -136,16 +147,16 @@ module.exports = {
         });
     },
     getInternetConnectedAdaptor: function getInternetConnectedAdaptor() {
-        var _this6 = this;
+        var _this7 = this;
 
         return new _bluebird2.default(function (resolve, reject) {
-            _this6.exec(_util2.default.format('"%s" "Get-NetConnectionProfile"', getPowershell())).then(function (output) {
+            _this7.exec(_util2.default.format('"%s" "Get-NetConnectionProfile"', _this7.getPowershell())).then(function (output) {
                 var networkData = output.split('\n').map(Function.prototype.call, String.prototype.trim).filter(Boolean);
                 var statusObject = {};
                 networkData.forEach(function (statusItem) {
                     statusObject[statusItem.split(':')[0].trim()] = statusItem.split(':')[1].trim();
                 });
-                _this6.getLocalIp(statusObject.InterfaceIndex).then(function (ip) {
+                _this7.getLocalIp(statusObject.InterfaceIndex).then(function (ip) {
                     statusObject['ip'] = ip;
                     resolve(statusObject);
                 });
@@ -153,19 +164,19 @@ module.exports = {
         });
     },
     getLocalIp: function getLocalIp(AdapterID) {
-        var _this7 = this;
+        var _this8 = this;
 
         return new _bluebird2.default(function (resolve) {
-            _this7.exec(_util2.default.format('"%s" "(Get-NetAdapter -ifIndex "%s" | Get-NetIPAddress).IPv4Address"', getPowershell(), AdapterID)).then(function (output) {
+            _this8.exec(_util2.default.format('"%s" "(Get-NetAdapter -ifIndex "%s" | Get-NetIPAddress).IPv4Address"', _this8.getPowershell(), AdapterID)).then(function (output) {
                 resolve(output.replace('\n', '').replace('\r', ''));
             });
         });
     },
     getClients: function getClients(opts) {
-        var _this8 = this;
+        var _this9 = this;
 
         return new _bluebird2.default(function (resolve, reject) {
-            _this8.getStatus().then(function (statusObject) {
+            _this9.getStatus().then(function (statusObject) {
                 resolve({
                     connected: statusObject.Status === 'Started' ? parseInt(statusObject['Number of clients']) : 0,
                     max: parseInt(statusObject['Max number of clients'])
@@ -174,13 +185,13 @@ module.exports = {
         });
     },
     create: function create(name, key) {
-        var _this9 = this;
+        var _this10 = this;
 
         console.log('Configuring hotspot with SSID:', name);
 
         return new _bluebird2.default(function (resolve, reject) {
-            _this9.exec('netsh wlan set hostednetwork mode=allow').then(function () {
-                return _this9.exec(_util2.default.format('netsh wlan set hostednetwork ssid="%s" key="%s" keyUsage=temporary', name, key));
+            _this10.exec('netsh wlan set hostednetwork mode=allow').then(function () {
+                return _this10.exec(_util2.default.format('netsh wlan set hostednetwork ssid="%s" key="%s" keyUsage=temporary', name, key));
             }).then(resolve).catch(reject);
         });
     },
